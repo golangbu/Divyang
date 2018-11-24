794b6b83d76f58efd90460baf8db19a73a36883c
Pruthwiraj S <pruthwi112@gmail.com>
2018-11-25 01:47:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pruthwirajs/divyang/Config.java
@@ -0,0 +1,6 @@
+package com.example.pruthwirajs.divyang;
+
+public class Config {
+    public static String EMAIL ="your-gmail-address";  //gmail address
+    public static String PASSWORD ="your-password"; //password
+}
